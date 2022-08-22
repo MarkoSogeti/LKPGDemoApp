e6f417e47496d86f1fe08ef26da9abe3ff90b793
Marko Marinkovic <marko.marinkovic@sogeti.com>
2022-08-22 13:15:04 +0200

    ...

--- src/service/auth-service.js
+++ src/service/auth-service.js
@@ -1,5 +1,5 @@
 export default async function getUserInfo() {
-    const response = await fetch('https://icy-cliff-03f1e8d03.1.azurestaticapps.net/.auth/me');
+    const response = await fetch('https://ashy-plant-08f9bee03.1.azurestaticapps.net/.auth/me');
     const payload = await response.json();
     const { clientPrincipal } = payload;
     return clientPrincipal;
